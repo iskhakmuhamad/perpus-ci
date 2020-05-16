bdb449aa8e8b921b63b1ff131741e1554503297f
iskhakmuhamad <iskhakmuhama@gmail.com>
2020-05-16 17:24:26 +0700

    add database

---
+++ database/db_perpus.sql
@@ -0,0 +1,60 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost
+-- Generation Time: May 16, 2020 at 12:22 PM
+-- Server version: 10.4.11-MariaDB
+-- PHP Version: 7.4.3
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `db_perpus`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_anggota`
+--
+-- Error reading structure for table db_perpus.tbl_anggota: #1932 - Table 'db_perpus.tbl_anggota' doesn't exist in engine
+-- Error reading data for table db_perpus.tbl_anggota: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `db_perpus`.`tbl_anggota`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_buku`
+--
+-- Error reading structure for table db_perpus.tbl_buku: #1932 - Table 'db_perpus.tbl_buku' doesn't exist in engine
+-- Error reading data for table db_perpus.tbl_buku: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `db_perpus`.`tbl_buku`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_transaksi`
+--
+-- Error reading structure for table db_perpus.tbl_transaksi: #1932 - Table 'db_perpus.tbl_transaksi' doesn't exist in engine
+-- Error reading data for table db_perpus.tbl_transaksi: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `db_perpus`.`tbl_transaksi`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_user`
+--
+-- Error reading structure for table db_perpus.tbl_user: #1932 - Table 'db_perpus.tbl_user' doesn't exist in engine
+-- Error reading data for table db_perpus.tbl_user: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `db_perpus`.`tbl_user`' at line 1
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
